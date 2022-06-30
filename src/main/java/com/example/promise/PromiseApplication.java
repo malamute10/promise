3fba02e51c3355d0cafff042ae8eed746ea63091
yein-lee <agop0310@gmail.com>
2022-06-30 15:46:46 +0900

    feat: 최초 세팅

---
+++ src/main/java/com/example/promise/PromiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.promise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromiseApplication.class, args);
+	}
+
+}
